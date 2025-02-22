b0d2b42d1799a24adc06c2a69395f062fce7783c
dbonifaz <diego.b1205@hotmail.com>
2025-02-22 12:21:55 -0500

    caso 5

---
+++ caso (5)/utils/retryPolicy.js
@@ -0,0 +1,13 @@
+const retry = async (fn, retries = 3, delay = 1000) => {
+  try {
+    return await fn();
+  } catch (error) {
+    if (retries > 0) {
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      return retry(fn, retries - 1, delay);
+    }
+    throw error;
+  }
+};
+
+module.exports = retry;
