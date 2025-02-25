72fe5bc046ee461946258dfdf2f658a3ac569384
Ricardo Tipan <ricardojaviertipan@gmail.com>
2025-02-25 00:14:10 -0500

    Move files on folder

--- caso (7)/src/infraestructura/configuracion/temas.ts
+++
@@ -1,9 +0,0 @@
-// Definimos una constante con tipos explícitos
-const Temas: { [key: string]: string } = {
-    TELEGRAPHO: 'telegrafo',
-    NOTIFICATIONS: 'notifications',
-    LOGS: 'logs'
-};
-
-// Exportamos la constante
-export default Temas;
